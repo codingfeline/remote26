551b95bfcfed65baa03a676c92a2ea0de1c9861d
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-05 21:11:35 +0100

    property containter

---
+++ app/customer/_components/ItemContainer.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+  title?: string
+}
+
+const ItemContainer = ({ children, title }: Props) => {
+  return (
+    <>
+      <h2 className="text-xl font-semibold mb-1">{title}</h2>
+      <div className="grid  lg:grid-cols-2 grid-cols-1 gap-3">{children}</div>
+    </>
+  )
+}
+
+export default ItemContainer
