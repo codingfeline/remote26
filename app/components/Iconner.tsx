7a0acc2b1af233fd3f0d0f780e0f949e6c8aba1a
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-05 21:09:50 +0100

    icon wrapper

---
+++ app/components/Iconner.tsx
@@ -0,0 +1,19 @@
+import { IconProps } from '@radix-ui/themes'
+import { ComponentType } from 'react'
+
+interface Props {
+  Icon: ComponentType<IconProps>
+  func?: () => void
+}
+
+const Iconner = ({ Icon, func }: Props) => {
+  return (
+    <div
+      className={`text-2xl  ${func ? 'text-gray-500 hover:text-violet-700 cursor-pointer' : 'text-gray-700'}`}
+    >
+      {<Icon onClick={func} />}
+    </div>
+  )
+}
+
+export default Iconner
