00700cbe967171512140c91e34192f3d943275e6
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-12 20:44:32 +0100

    prep method edit 3

--- app/customer/[id]/method/page.tsx
+++ app/customer/[id]/method/page.tsx
@@ -1,10 +1,10 @@
 import MainPage from '@/app/components/MainPage'
-import MethodForm from '../../_components/MethodForm'
 
 const MethodPage = () => {
   return (
     <MainPage>
-      <MethodForm />
+      {/* <MethodForm /> */}
+      PLACEHOLDER
     </MainPage>
   )
 }
