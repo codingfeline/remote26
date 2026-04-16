f65334208ee14bab6c9bf7d571d86510dfcbdab9
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-16 22:34:18 +0100

    add zod

---
+++ app/api/schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const MethodInfoSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  url: z.url().optional(),
+  username: z.string().optional(),
+  password: z.string().optional(),
+  notes: z.string().optional(),
+});
