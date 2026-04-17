62e9de0dc3d733cc1cb9fdd8312c441d87efb20a
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-17 20:44:13 +0100

    add cust route

---
+++ app/api/cust/route.ts
@@ -0,0 +1,21 @@
+import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+
+export async function GET() {
+
+  try {
+    const customers = await prisma.customer.findMany({
+      select: {
+        id: true,
+        name: true,
+        solution: true,
+      }
+    })
+    return NextResponse.json(customers)
+  } catch {
+    return NextResponse.json({ error: 'Failed to fetch customers' }, { status: 500 })
+
+  }
+
+}
