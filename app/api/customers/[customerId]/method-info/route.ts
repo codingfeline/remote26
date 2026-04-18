a672f6e62a719e0dc9a1cf962567999bcdc38df3
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-18 17:07:08 +0100

    add created methodInfo

--- app/api/customers/[customerId]/method-info/route.ts
+++ app/api/customers/[customerId]/method-info/route.ts
@@ -1,32 +1,54 @@
-import { MethodInfoSchema } from '@/app/api/schema';
+import { CustIDProp, MethodInfoSchema } from '@/app/api/schema';
 import prisma from '@/lib/prisma';
+import { ObjectId } from 'mongodb';
 import { NextRequest, NextResponse } from 'next/server';
 import { z } from 'zod';
 
-export async function POST(req: NextRequest, { params }: { params: Promise<{ customerId: string }> }) {
-
-  const body = await req.json();
-  const data = MethodInfoSchema.parse(body);
-  const customerId = (await params).customerId
-
+export async function POST(req: NextRequest, { params }: CustIDProp) {
   try {
-    // create new method info
-    const newMethod = {
-      id: crypto.randomUUID(),
-      ...data,
-    };
+    const body = await req.json();
+    const { customerId } = await params
+    // validate request body
+    const data = MethodInfoSchema.parse(body);
 
-    // update customer with new method info
+    const customer = await prisma.customer.findUnique({ where: { id: customerId } });
+    if (!customer) {
+      return NextResponse.json({ error: 'Customer not found' }, { status: 404 });
+    }
+
+    // create new method info
+    const newMethodInfo = {
+      id: new ObjectId().toString(),
+      methodName: data.methodName,
+      url: data.url ?? "",
+      username: data.username ?? "",
+      password: data.password ?? "",
+      notes: data.notes ?? "",
+    }
+
+    // add new method info to existing list
     await prisma.customer.update({
       where: { id: customerId },
-      data: newMethod,
-    });
+      data: {
+        methodInfo: [
+          ...customer.methodInfo,
+          newMethodInfo,
+        ]
+      }
+    })
+    return NextResponse.json(newMethodInfo, { status: 201 });
 
-    return NextResponse.json({ success: true, methodId: newMethod.id });
   } catch (error) {
     if (error instanceof z.ZodError) {
-      return NextResponse.json({ error: z.flattenError(error) }, { status: 400 });
+      return NextResponse.json(
+        { error: z.flattenError(error), message: 'Validation failed' },
+        { status: 400 }
+      );
     }
-    return NextResponse.json({ error: 'Failed to add method' }, { status: 500 });
+    console.error(error)
+    return NextResponse.json(
+      { error: 'Failed to add method' },
+      { status: 500 }
+    );
   }
 }
