c8bcc1f8b34e5798987632b35c754a303d31fdf8
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-23 16:08:12 +0100

    add MyButton options

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,16 +1,25 @@
 'use client'
 
+import { IconProps } from '@radix-ui/themes'
 import Link from 'next/link'
+import { ComponentType } from 'react'
 
 interface Prop {
-  url: string
+  url?: string
   label: string
+  Icon?: ComponentType<IconProps>
+  children?: React.ReactNode
+  iconPosition?: 'left' | 'right'
 }
 
-const MyButton = ({ url, label }: Prop) => {
+const MyButton = ({ url, label, Icon, iconPosition }: Prop) => {
   return (
-    <button className="cursor-pointer bg-violet-200 p-1 hover:bg-violet-300 px-2 rounded-sm  ">
-      <Link href={`${url}`}>{label}</Link>
+    <button className=" flex items-center gap-2 justify-center ">
+      <div className="cursor-pointer bg-violet-200 p-1 hover:bg-violet-300 px-2 rounded-sm flex items-center gap-2 justify-center ">
+        {iconPosition === 'left' && Icon && <Icon />}
+        {url ? <Link href={`${url}`}>{label}</Link> : label}
+        {iconPosition === 'right' && Icon && <Icon />}
+      </div>
     </button>
   )
 }
