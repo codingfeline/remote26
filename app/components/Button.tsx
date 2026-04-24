28711fd3c17f529cb6d61a1e1c0b247b14ec0f92
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-24 14:41:48 +0100

    add onClick prop

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -10,11 +10,12 @@ interface Prop {
   Icon?: ComponentType<IconProps>
   children?: React.ReactNode
   iconPosition?: 'left' | 'right'
+  click?: () => void
 }
 
-const MyButton = ({ url, label, Icon, iconPosition }: Prop) => {
+const MyButton = ({ url, label, Icon, iconPosition, click }: Prop) => {
   return (
-    <button className=" flex items-center gap-2 justify-center ">
+    <button className=" flex items-center gap-2 justify-center" onClick={click}>
       <div className="cursor-pointer bg-violet-200 p-1 hover:bg-violet-300 px-2 rounded-sm flex items-center gap-2 justify-center ">
         {iconPosition === 'left' && Icon && <Icon />}
         {url ? <Link href={`${url}`}>{label}</Link> : label}
