f68b705a04fa74bf824c96d83b24c5ffb62c4ccf
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-25 18:18:46 +0100

    add secondary mode

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -11,12 +11,15 @@ interface Prop {
   children?: React.ReactNode
   iconPosition?: 'left' | 'right'
   click?: () => void
+  secondary?: boolean
 }
 
-const MyButton = ({ url, label, Icon, iconPosition, click }: Prop) => {
+const MyButton = ({ url, label, Icon, iconPosition, click, secondary }: Prop) => {
   return (
-    <button className=" flex items-center gap-2 justify-center" onClick={click}>
-      <div className="cursor-pointer bg-violet-200 p-1 hover:bg-violet-300 px-2 rounded-sm flex items-center gap-2 justify-center ">
+    <button className={`flex items-center gap-2 justify-center `} onClick={click}>
+      <div
+        className={` ${secondary ? 'bg-gray-300 hover:border-gray-400 border-gray-300 border' : 'bg-violet-200 hover:bg-violet-300'} p-1 cursor-pointer  px-2 rounded-sm flex items-center gap-2 justify-center `}
+      >
         {iconPosition === 'left' && Icon && <Icon />}
         {url ? <Link href={`${url}`}>{label}</Link> : label}
         {iconPosition === 'right' && Icon && <Icon />}
