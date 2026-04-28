fdf1cab9dfea3794665b882ee08ae0b09a610cb8
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-28 22:59:16 +0100

    add device display

---
+++ app/customer/_components/DeviceInfo.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { Copy, Pencil } from '@/app/components'
+import { useCopyToClipboard } from '@/app/components/CopyToClipboard'
+import Iconner from '@/app/components/Iconner'
+import { DevicePassword as Device } from '@prisma/client'
+import ItemContainer from './ItemContainer'
+
+interface Props {
+  devicePasswords: Device[]
+  cid: string
+}
+
+const DeviceInfo = ({ devicePasswords: device, cid }: Props) => {
+  const { copy: handleCopy } = useCopyToClipboard()
+  if (!device) return
+
+  return (
+    <section className="compo">
+      <ItemContainer title="Device">
+        {device.map(s => (
+          <div key={s.id} className="border rounded-xl p-4">
+            <Iconner href={`/customer/${cid}/contact/${s.id}`} Icon={Pencil} />
+            <p>
+              <strong>Make:</strong> {s.make}
+            </p>
+            <p>
+              <strong>username:</strong> {s.username}
+              <Iconner Icon={Copy} func={() => handleCopy(s.username)} />
+            </p>
+            <p>
+              <strong>password:</strong> {s.password}
+              <Iconner Icon={Copy} func={() => handleCopy(s.password)} />
+            </p>
+          </div>
+        ))}
+      </ItemContainer>
+    </section>
+  )
+}
+
+export default DeviceInfo
