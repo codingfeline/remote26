69287ede78a06db1d96916b5b67a706426035836
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-30 14:40:36 +0100

    add scanToFolder page

---
+++ app/customer/_components/ScanToFolder.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { Copy, Pencil } from '@/app/components'
+import { useCopyToClipboard } from '@/app/components/CopyToClipboard'
+import Iconner from '@/app/components/Iconner'
+import { ScanToFolder } from '@prisma/client'
+import ItemContainer from './ItemContainer'
+
+interface Props {
+  scan2e: ScanToFolder[]
+  cid: string
+}
+
+const ScanToFolderInfo = ({ scan2e, cid }: Props) => {
+  const { copy: handleCopy } = useCopyToClipboard()
+  if (!scan2e) return
+
+  return (
+    <section className="compo">
+      <ItemContainer title="Scan To Folder">
+        {scan2e.map(s => (
+          <div key={s.id} className="border rounded-xl p-4">
+            <Iconner href={`/customer/${cid}/contact/${s.id}`} Icon={Pencil} />
+            <p>
+              <strong>hostname:</strong> {s.hostname}
+              {s.hostname && <Iconner Icon={Copy} func={() => handleCopy(s.hostname)} />}
+            </p>
+            <p>
+              <strong>folder:</strong> {s.folder}
+              {s.folder && <Iconner Icon={Copy} func={() => handleCopy(s.folder)} />}
+            </p>
+            <p>
+              <strong>username:</strong> {s.username}
+              {s.username && <Iconner Icon={Copy} func={() => handleCopy(s.username)} />}
+            </p>
+            <p>
+              <strong>password:</strong> {s.password}
+              {s.password && <Iconner Icon={Copy} func={() => handleCopy(s.password)} />}
+            </p>
+          </div>
+        ))}
+      </ItemContainer>
+    </section>
+  )
+}
+
+export default ScanToFolderInfo
