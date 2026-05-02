bdebf12019fbb1edab0466ca14111c75c988010c
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-05-02 09:00:54 +0100

    add server page

---
+++ app/customer/[id]/server/new/page.tsx
@@ -0,0 +1,12 @@
+import { MainPage } from '@/app/components'
+import ServerForm from '@/app/customer/_components/ServerForm'
+
+const NewServerPage = () => {
+  return (
+    <MainPage>
+      <ServerForm />
+    </MainPage>
+  )
+}
+
+export default NewServerPage
