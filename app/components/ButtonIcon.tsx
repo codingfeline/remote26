af64f54411bf45ae83b3f15235a52cad3e2c3efe
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-05-02 20:51:28 +0100

    button white text for visibility

--- app/components/ButtonIcon.tsx
+++ app/components/ButtonIcon.tsx
@@ -15,7 +15,7 @@ const ButtonIcon = ({ href, Icon, children, classes, active }: Prop) => {
     <div className={`flex  ${classes}`}>
       <Link
         href={href}
-        className={`${active ? 'bg-violet-400 hover:bg-violet-500' : 'bg-violet-200 hover:bg-violet-300'} flex items-center gap-2 p-1 px-2 rounded-sm`}
+        className={`${active ? 'bg-violet-400 hover:bg-violet-500 text-white' : 'bg-violet-200 hover:bg-violet-300'} flex items-center gap-2 p-1 px-2 rounded-sm`}
       >
         {Icon && <Icon />} {children}
       </Link>
