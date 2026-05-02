e21b43e3a64d344b46f4698baccd73b9f3d5923f
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-05-02 14:30:32 +0100

    add delete

--- app/customer/_components/DeviceInfo.tsx
+++ app/customer/_components/DeviceInfo.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { Copy, Pencil } from '@/app/components'
+import { ConfirmDelete, Copy, Pencil } from '@/app/components'
 import { useCopyToClipboard } from '@/app/components/CopyToClipboard'
 import Iconner from '@/app/components/Iconner'
 import { DevicePassword as Device } from '@prisma/client'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 import ItemContainer from './ItemContainer'
 
 interface Props {
@@ -13,6 +15,13 @@ interface Props {
 
 const DeviceInfo = ({ devicePasswords: device, cid }: Props) => {
   const { copy: handleCopy } = useCopyToClipboard()
+  const router = useRouter()
+
+  const handleDelete = async (id: string) => {
+    await axios.delete(`/api/customers/${cid}/device/${id}`)
+    router.refresh()
+  }
+
   if (!device) return
 
   return (
@@ -20,13 +29,16 @@ const DeviceInfo = ({ devicePasswords: device, cid }: Props) => {
       <ItemContainer title="Device">
         {device.map(s => (
           <div key={s.id} className="border rounded-xl p-4">
-            <Iconner href={`/customer/${cid}/contact/${s.id}`} Icon={Pencil} />
+            <div className="flex justify-between">
+              <Iconner href={`/customer/${cid}/device/${s.id}`} Icon={Pencil} />
+              <ConfirmDelete onConfirm={() => handleDelete(s.id)} />
+            </div>
             <p>
               <strong>Make:</strong> {s.make}
             </p>
             <p>
               <strong>username:</strong> {s.username}
-              <Iconner Icon={Copy} func={() => handleCopy(s.username)} />
+              {s.username && <Iconner Icon={Copy} func={() => handleCopy(s.username)} />}
             </p>
             <p>
               <strong>password:</strong> {s.password}
