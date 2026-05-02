52ce76384ab6db7a573607012b67afc7689467e7
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-05-02 15:24:59 +0100

    add new customer route

--- app/api/customers/route.ts
+++ app/api/customers/route.ts
@@ -1,9 +1,9 @@
+import { CustomerSchema } from '@/app/schema';
 import prisma from '@/lib/prisma';
-import { NextResponse } from 'next/server';
-
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
 
 export async function GET() {
-
   try {
     const customers = await prisma.customer.findMany({
       select: {
@@ -16,7 +16,32 @@ export async function GET() {
     return NextResponse.json(customers)
   } catch {
     return NextResponse.json({ error: 'Failed to fetch customers' }, { status: 500 })
-
   }
+}
 
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const data = CustomerSchema.parse(body);
+
+    const customer = await prisma.customer.create({
+      data: {
+        name: data.name,
+        solution: data.solution,
+        date: new Date(),
+        version: 0,
+      }
+    })
+
+    return NextResponse.json(customer, { status: 201 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { error: z.flattenError(error), message: 'Validation failed' },
+        { status: 400 }
+      );
+    }
+    console.error(error)
+    return NextResponse.json({ error: 'Failed to create customer' }, { status: 500 });
+  }
 }
