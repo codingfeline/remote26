b339b0bf40b65a435f9b6986ad02cd7a249cc74e
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-05-02 14:19:33 +0100

    scanToEmail POST and DELETE

---
+++ app/api/customers/[customerId]/scan-to-email/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse, ObjectId, prisma } from '@/app/api';
+import { CustomerAllProps, ScanToEmailSchema } from '@/app/schema';
+import { z } from 'zod';
+
+export async function POST(req: NextRequest, { params }: CustomerAllProps) {
+  try {
+    const body = await req.json();
+    const { customerId } = await params
+
+    const data = ScanToEmailSchema.parse(body);
+
+    const customer = await prisma.customer.findUnique({ where: { id: customerId } });
+    if (!customer) {
+      return NextResponse.json({ error: 'Customer not found' }, { status: 404 });
+    }
+
+    const newEntry = {
+      id: new ObjectId().toString(),
+      hostname: data.hostname ?? '',
+      username: data.username ?? '',
+      password: data.password ?? '',
+      port: data.port ?? '',
+    }
+
+    await prisma.customer.update({
+      where: { id: customerId },
+      data: { scanToEmail: [...customer.scanToEmail, newEntry] },
+    })
+
+    return NextResponse.json(newEntry, { status: 201 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { error: z.flattenError(error), message: 'Validation failed' },
+        { status: 400 }
+      );
+    }
+    console.error(error)
+    return NextResponse.json({ error: 'Failed to add scan-to-email' }, { status: 500 });
+  }
+}
