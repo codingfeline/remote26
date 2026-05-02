99e127af4689d381aac9f4d23f4fc1e6fcdc2515
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-05-02 17:41:16 +0100

    conditional display

--- app/customer/_components/filterCustomersList.tsx
+++ app/customer/_components/filterCustomersList.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { Eye, MyButton } from '@/app/components'
 import ButtonIcon from '@/app/components/ButtonIcon'
 import { Box, Container, Flex, TextField } from '@radix-ui/themes'
+import { usePathname } from 'next/navigation'
 import { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 
@@ -13,10 +15,12 @@ interface names {
 interface Prop {
   names: names[]
   requireSearch?: boolean
+  horizontal?: boolean
 }
 
-const FilterCustomersList = ({ names, requireSearch }: Prop) => {
+const FilterCustomersList = ({ names, requireSearch, horizontal }: Prop) => {
   const [searchTerm, setSearchTerm] = useState('')
+  const pathname = usePathname()
 
   const filtered = names.filter(t =>
     t.name.toLowerCase().includes(searchTerm.toLowerCase()),
@@ -24,33 +28,52 @@ const FilterCustomersList = ({ names, requireSearch }: Prop) => {
   // const showList = requireSearch ? searchTerm !== '' : true
 
   return (
-    <div>
-      <Flex my="2">
-        {/* <Flex my="2" className="place-self-center py-1" direction="column"> */}
-        <Container size="3">
-          <Box width="5" className="max-w-5xl">
+    <div className="w-full">
+      <MyButton secondary label="Add Customer" url="/customer/new" />
+      {horizontal ? (
+        <div className="my-2">
+          <div className="mb-2">
             <TextField.Root
-              placeholder="find customer"
+              placeholder="filter customer"
               onChange={e => setSearchTerm(e.target.value)}
             >
               <TextField.Slot>
                 <FaSearch height="16" width="16" />
               </TextField.Slot>
             </TextField.Root>
-          </Box>
-
-          {
-            // showlist &&
-            filtered.map(t => (
-              <div key={t.id}>
-                <Flex my="1" wrap="nowrap">
-                  <ButtonIcon href={`/customer/${t.id}`}>{t.name}</ButtonIcon>
-                </Flex>
+          </div>
+          <div className="flex flex-wrap gap-2">
+            {filtered.map(t => (
+              <div key={t.id} className="flex items-center">
+                <ButtonIcon href={`/customer/${t.id}`}>{t.name}</ButtonIcon>
               </div>
-            ))
-          }
-        </Container>
-      </Flex>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <Flex my="2">
+          <Container size="3">
+            <Box width="5" className="max-w-5xl">
+              <TextField.Root
+                placeholder="filter customer"
+                onChange={e => setSearchTerm(e.target.value)}
+              >
+                <TextField.Slot>
+                  <FaSearch height="16" width="16" />
+                </TextField.Slot>
+              </TextField.Root>
+            </Box>
+            {filtered.map(t => (
+              <Flex key={t.id} my="1" wrap="nowrap" align="center">
+                <ButtonIcon href={`/customer/${t.id}`}>{t.name}</ButtonIcon>
+                {pathname === `/customer/${t.id}` && (
+                  <Eye className="text-violet-600 ml-1 shrink-0" />
+                )}
+              </Flex>
+            ))}
+          </Container>
+        </Flex>
+      )}
     </div>
   )
 }
